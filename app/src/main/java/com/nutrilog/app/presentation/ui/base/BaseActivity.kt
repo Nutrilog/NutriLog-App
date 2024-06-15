aca43aba19d9f7da2a6e1642cb2cc2167eda4728
Nizar Izzuddin Yatim Fadlan <fadlannizar5@gmail.com>
2024-06-15 10:28:56 +0700

    fix(ui): Force light mode

--- app/src/main/java/com/nutrilog/app/presentation/ui/base/BaseActivity.kt
+++ app/src/main/java/com/nutrilog/app/presentation/ui/base/BaseActivity.kt
@@ -2,13 +2,14 @@ package com.nutrilog.app.presentation.ui.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
+import androidx.core.view.updateLayoutParams
 import androidx.viewbinding.ViewBinding
 import com.nutrilog.app.domain.model.Language
 import com.nutrilog.app.presentation.ui.main.profile.ProfileViewModel
@@ -32,6 +33,7 @@ abstract class BaseActivity<viewBinding : ViewBinding> : AppCompatActivity() {
         setupEdgeToEdge()
         onViewBindingCreated(savedInstanceState)
         observe(profileViewModel.getLanguage(), ::setLanguage)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
     protected open fun onViewBindingCreated(savedInstanceState: Bundle?) {}
@@ -41,12 +43,12 @@ abstract class BaseActivity<viewBinding : ViewBinding> : AppCompatActivity() {
         binding.apply {
             ViewCompat.setOnApplyWindowInsetsListener(root) { v, insets ->
                 val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.updatePadding(
-                    systemBars.left,
-                    systemBars.top,
-                    systemBars.right,
-                    systemBars.bottom,
-                )
+                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    leftMargin = systemBars.left
+                    bottomMargin = systemBars.bottom
+                    rightMargin = systemBars.right
+                    topMargin = systemBars.top
+                }
 
                 WindowInsetsCompat.CONSUMED
             }
