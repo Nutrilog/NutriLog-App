8d05670dff16bfc01020ce7a9e117543bd702ff2
Nizar Izzuddin Yatim Fadlan <fadlannizar5@gmail.com>
2024-06-21 11:13:03 +0700

    Release v1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.4"
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_BASE_URL", "\"https://capstone-ijhyluyd4q-et.a.run.app/\"")
